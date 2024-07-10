2b789f3b340086fbc44c23f8e67e9fb9c5dd7419
Paulo Salvatore <salvatoregames@gmail.com>
2024-07-10 20:48:28 -0300

    Implementação do botão voltar

--- app/src/main/java/com/oceanbrasil/ocean_android_intro_julho2024/ResultadoActivity.kt
+++ app/src/main/java/com/oceanbrasil/ocean_android_intro_julho2024/ResultadoActivity.kt
@@ -1,6 +1,7 @@
 package com.oceanbrasil.ocean_android_intro_julho2024
 
 import android.os.Bundle
+import android.widget.Button
 import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
@@ -22,5 +23,10 @@ class ResultadoActivity : AppCompatActivity() {
 
         val tvResultado = findViewById<TextView>(R.id.tvResultado)
         tvResultado.text = nomeDigitado
+
+        val btVoltar = findViewById<Button>(R.id.btVoltar)
+        btVoltar.setOnClickListener {
+            finish()
+        }
     }
 }
